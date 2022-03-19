2a1b6eb5ed4b7dbefbacdd9e11ef8be5ed8a8ac1
Gustavo Baierski <gustavobaierski@gmail.com>
2022-03-19 11:17:13 -0300

    Resolver exercício Lanche

---
+++ 9_lanche.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main() {
+    
+    int codigo, quantidade;
+    double preco = 0;
+    
+    scanf("%d %d", &codigo ,&quantidade);
+    
+    if (codigo == 1) 
+        preco = 4.00;
+
+    if (codigo == 2)
+        preco = 4.50;
+
+    if (codigo == 3)
+        preco = 5.00;
+        
+    if (codigo == 4)
+        preco = 2.00;
+        
+    if (codigo == 5)
+        preco = 1.50;
+    
+    printf("Total: R$ %.2lf\n", preco * quantidade);
+}
