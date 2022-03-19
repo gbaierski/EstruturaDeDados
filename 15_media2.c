c39229bc39e00c61a6d75722beff37a19fbe03fd
Gustavo Baierski <gustavobaierski@gmail.com>
2022-03-19 14:50:59 -0300

    Resolver exercício Média 2

---
+++ 15_media2.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    double a, b, c, media;
+
+    scanf("%lf %lf %lf", &a, &b, &c);
+    media = (a * 0.2 + b * 0.3 + c * 0.5);
+    printf("MEDIA = %.1lf\n", media);
+    
+    return 0;
+}
